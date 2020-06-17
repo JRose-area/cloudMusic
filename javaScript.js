f4d4e40b5874cc94479f711a708a795da19d9756
jianghan <jianghan@jianghan.com>
2020-06-17 11:23:11 +0800

    first commit

---
+++ javaScript.js
@@ -0,0 +1,28 @@
+window.onload = function() {
+    var mySwiper = new Swiper ('.swiper-container', {
+        // direction: 'vertical', // 垂直切换选项
+        loop: true, // 循环模式选项
+        autoplay: 
+        {
+            delay: 3000,
+            stopOnLastSlide: false,
+            disableOnInteraction: true,
+            },
+        
+        // 如果需要分页器
+        pagination: {
+          el: '.swiper-pagination',
+        },
+        
+        // 如果需要前进后退按钮
+        navigation: {
+          nextEl: '.swiper-button-next',
+          prevEl: '.swiper-button-prev',
+        },
+        
+        // 如果需要滚动条
+        scrollbar: {
+          el: '.swiper-scrollbar',
+        },
+      })        
+  }
